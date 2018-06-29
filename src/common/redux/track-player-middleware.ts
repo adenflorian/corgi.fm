7cef4199e9d94d6280cf3767b7d06e2953350b9f
David Valachovic <David.E.Valachovic@gmail.com>
2018-06-28 22:47:17 -0500

    track starts playing on refresh

--- src/common/redux/track-player-middleware.ts
+++ src/common/redux/track-player-middleware.ts
@@ -2,7 +2,7 @@ import {AnyAction, Dispatch, Middleware, Store} from 'redux'
 import {audioContext} from '../../client/setup-audio-context'
 import {TrackPlayer} from '../../client/TrackPlayer'
 import {makeActionCreator, makeBroadcaster, makeServerAction} from './redux-utils'
-import {selectTrack, setTrackIndex} from './tracks-redux'
+import {ITracks, selectTrack, setTrackIndex, UPDATE_TRACKS} from './tracks-redux'
 
 export const PLAY_TRACK = 'PLAY_TRACK'
 export const playTrack = makeServerAction(makeBroadcaster(makeActionCreator(PLAY_TRACK, 'id')))
@@ -31,6 +31,7 @@ export const trackPlayerMiddleware: Middleware = (store: Store) => next => actio
 		case STOP_TRACK:
 		case TOGGLE_PLAY_TRACK:
 		case RESTART_TRACK:
+		case UPDATE_TRACKS:
 			// case REFRESH_TRACK_PLAYER_EVENTS:
 			let trackPlayer = trackPlayers[action.id]
 			if (trackPlayer === undefined) {
@@ -54,21 +55,31 @@ function foo(action: AnyAction, trackPlayer: TrackPlayer, next: Dispatch, store:
 		case STOP_TRACK:
 			trackPlayer.stop()
 			return next(action)
-		case TOGGLE_PLAY_TRACK:
-			if (trackPlayer.isPlaying()) {
-				trackPlayer.stop()
-				next(stopTrack())
-			} else {
-				trackPlayer.play(selectTrack(store.getState(), action.id).notes.length)
-				next(playTrack())
-			}
-			return next(action)
+		// case TOGGLE_PLAY_TRACK:
+		// 	if (trackPlayer.isPlaying()) {
+		// 		trackPlayer.stop()
+		// 		next(stopTrack())
+		// 	} else {
+		// 		trackPlayer.play(selectTrack(store.getState(), action.id).notes.length)
+		// 		next(playTrack())
+		// 	}
+		// 	return next(action)
 		case RESTART_TRACK:
 			if (trackPlayer.isPlaying()) {
 				trackPlayer.stop()
 				trackPlayer.play(selectTrack(store.getState(), action.id).notes.length)
 			}
 			return next(action)
+		case UPDATE_TRACKS:
+			next(action)
+			const tracks: ITracks = action.tracks
+			Object.keys(tracks).forEach(trackId => {
+				const track = tracks[trackId]
+				if (track.isPlaying) {
+					store.dispatch(playTrack(track.id))
+				}
+			})
+			return
 		// case REFRESH_TRACK_PLAYER_EVENTS:
 		// 	next(action)
 		// 	trackPlayer.setEvents(selectTrack(store.getState(), action.id).notes.length)
