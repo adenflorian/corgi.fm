980ccec0dd0798ec2dc860d0c03bd76d288bb842
David Valachovic <David.E.Valachovic@gmail.com>
2018-07-06 20:57:24 -0500

    changed so source maps aren't seen in prod and made new public folder

--- src/server/setup-express-app.ts
+++ src/server/setup-express-app.ts
@@ -3,10 +3,10 @@ import * as path from 'path'
 import {Store} from 'redux'
 
 export function setupExpressApp(app: express.Application, store: Store) {
-	app.use(express.static(path.join(__dirname, '../client')))
+	app.use(express.static(path.join(__dirname, '../public')))
 
 	app.get('/', (_, res) => {
-		res.sendFile(path.join(__dirname, '../client/index.html'))
+		res.sendFile(path.join(__dirname, '../public/index.html'))
 	})
 
 	app.get('/state', (_, res) => {
