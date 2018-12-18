d8d4269b75a28d4b1c6c1ee828f61daab678e46a
David Valachovic <David.E.Valachovic@gmail.com>
2018-12-17 21:37:34 -0600

    refactor rooms-redux

--- src/common/redux/rooms-redux.ts
+++ src/common/redux/rooms-redux.ts
@@ -1,23 +1,27 @@
-import {createReducer, SERVER_ACTION} from './redux-utils'
+import {List} from 'immutable'
+import {Reducer} from 'redux'
+import {SERVER_ACTION} from './redux-utils'
+
+type RoomNames = List<string>
 
 export const SET_ROOMS = 'SET_ROOMS'
 export type SetRoomsAction = ReturnType<typeof setRooms>
-export const setRooms = (rooms: string[]) => ({
-	type: SET_ROOMS,
+export const setRooms = (rooms: RoomNames) => ({
+	type: SET_ROOMS as typeof SET_ROOMS,
 	rooms,
 })
 
 export const SET_ACTIVE_ROOM = 'SET_ACTIVE_ROOM'
 export type SetActiveRoomAction = ReturnType<typeof setActiveRoom>
 export const setActiveRoom = (room: string) => ({
-	type: SET_ACTIVE_ROOM,
+	type: SET_ACTIVE_ROOM as typeof SET_ACTIVE_ROOM,
 	room,
 })
 
 export const CHANGE_ROOM = 'CHANGE_ROOM'
 export type ChangeRoomAction = ReturnType<typeof changeRoom>
 export const changeRoom = (room: string) => ({
-	type: CHANGE_ROOM,
+	type: CHANGE_ROOM as typeof CHANGE_ROOM,
 	SERVER_ACTION,
 	room,
 })
@@ -25,7 +29,7 @@ export const changeRoom = (room: string) => ({
 export const CREATE_ROOM = 'CREATE_ROOM'
 export type CreateRoomAction = ReturnType<typeof createRoom>
 export const createRoom = (name: string) => ({
-	type: CREATE_ROOM,
+	type: CREATE_ROOM as typeof CREATE_ROOM,
 	SERVER_ACTION,
 	name,
 })
@@ -33,47 +37,52 @@ export const createRoom = (name: string) => ({
 export const REQUEST_CREATE_ROOM = 'REQUEST_CREATE_ROOM'
 export type RequestCreateRoomAction = ReturnType<typeof requestCreateRoom>
 export const requestCreateRoom = () => ({
-	type: REQUEST_CREATE_ROOM,
+	type: REQUEST_CREATE_ROOM as typeof REQUEST_CREATE_ROOM,
 	SERVER_ACTION,
 })
 
 export const DELETE_ROOM = 'DELETE_ROOM'
 export type DeleteRoomAction = ReturnType<typeof deleteRoom>
 export const deleteRoom = (name: string) => ({
-	type: DELETE_ROOM,
+	type: DELETE_ROOM as typeof DELETE_ROOM,
 	SERVER_ACTION,
 	name,
 })
 
 export interface IRoomsState {
-	all: string[],
+	names: RoomNames,
 	activeRoom: string
 }
 
 export type RoomsReduxAction = SetRoomsAction | SetActiveRoomAction |
 	ChangeRoomAction | CreateRoomAction | DeleteRoomAction
 
-export function roomsReducer(state: IRoomsState = {all: [], activeRoom: ''}, action: RoomsReduxAction): IRoomsState {
-	return {
-		...roomsOtherReducer({activeRoom: state.activeRoom}, action),
-		all: roomsArrayReducer(state ? state.all : undefined, action),
+const initialState: IRoomsState = {
+	names: List<string>(),
+	activeRoom: '',
+}
+
+export const roomsReducer: Reducer<IRoomsState, RoomsReduxAction> = (state = initialState, action) => ({
+	...roomsOtherReducer({activeRoom: state.activeRoom}, action),
+	names: roomsNamesReducer(state ? state.names : undefined, action),
+})
+
+const roomsOtherReducer: Reducer<{activeRoom: string}, RoomsReduxAction> = (state = {activeRoom: ''}, action) => {
+	switch (action.type) {
+		case SET_ACTIVE_ROOM: return {...state, activeRoom: action.room}
+		default: return state
 	}
 }
 
-const roomsOtherReducer = createReducer({activeRoom: ''},
-	{
-		[SET_ACTIVE_ROOM]: (state, {room}: SetActiveRoomAction) => ({...state, activeRoom: room}),
-	},
-)
-
-const roomsArrayReducer = createReducer(new Array<string>(),
-	{
-		[CREATE_ROOM]: (state, {name}: CreateRoomAction) => state.concat(name),
-		[DELETE_ROOM]: (state, {name}: DeleteRoomAction) => state.filter(x => x !== name),
-		[SET_ROOMS]: ({}, {rooms}: SetRoomsAction) => rooms,
-	},
-)
+const roomsNamesReducer: Reducer<RoomNames, RoomsReduxAction> = (state = List<string>(), action) => {
+	switch (action.type) {
+		case CREATE_ROOM: return state.concat(action.name)
+		case DELETE_ROOM: return state.filter(x => x !== action.name)
+		case SET_ROOMS: return action.rooms
+		default: return state
+	}
+}
 
 const selectRoomsState = (state: {rooms: IRoomsState}) => state.rooms
-export const selectAllRooms = (state: {rooms: IRoomsState}) => selectRoomsState(state).all
+export const selectAllRooms = (state: {rooms: IRoomsState}) => selectRoomsState(state).names
 export const selectActiveRoom = (state: {rooms: IRoomsState}) => selectRoomsState(state).activeRoom
