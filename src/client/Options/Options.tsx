d6db0908f4f3771bae74f15f9557d47e70a57c73
David Valachovic <David.E.Valachovic@gmail.com>
2019-05-05 13:26:09 -0500

    changing Options to a funcion component

--- src/client/Options/Options.tsx
+++ src/client/Options/Options.tsx
@@ -7,76 +7,72 @@ import {ConnectedOption} from '../Option'
 import {ConnectedOptionCheckbox} from '../RoomSetting'
 import './Options.less'
 
-export class Options extends React.Component {
-	public state = {
-		showOptions: false,
-	}
+export const Options = React.memo(function _Options() {
+	const [visible, setVisible] = React.useState(false)
 
-	public render() {
-		return (
-			<Fragment>
-				<Button
-					buttonProps={{onClick: () => this.setState({showOptions: !this.state.showOptions})}}
+	return (
+		<Fragment>
+			<Button
+				buttonProps={{onClick: () => setVisible(!visible)}}
+			>
+				Options
+			</Button>
+			{visible &&
+				<Modal
+					onHide={() => setVisible(false)}
+					className="options"
 				>
-					Options
-				</Button>
-				{this.state.showOptions &&
-					<Modal
-						onHide={() => this.setState({showOptions: false})}
-						className="options"
-					>
-						<div className="modalSection localOptions">
-							<div className="modalSectionLabel">Local Options</div>
-							<div className="modalSectionSubLabel">won't affect anyone else</div>
-							<ConnectedOption
-								option={AppOptions.showNoteNamesOnKeyboard}
-								label="show note names on keyboard"
-							/>
-							<ConnectedOption
-								option={AppOptions.requireCtrlToScroll}
-								label="require control key to zoom (might be needed by laptop users)"
-							/>
-							<ConnectedOption
-								option={AppOptions.showNoteSchedulerDebug}
-								label="note scheduler debug: enable"
-							/>
-							<ConnectedOption
-								option={AppOptions.renderNoteSchedulerDebugWhileStopped}
-								label="note scheduler debug: keep rendering even when song is stopped"
-							/>
-							<ConnectedOption
-								option={AppOptions.graphics_fancyConnections}
-								label="graphics: enable fancy connections"
-							/>
-							<ConnectedOption
-								option={AppOptions.graphics_ECS}
-								label="graphics: enable ECS animations (sequencer time marker thing)"
-							/>
-							<ConnectedOption
-								option={AppOptions.graphics_expensiveZoomPan}
-								label="graphics: enable expensive/fancy zoom and pan (sharper render, but slower)"
-							/>
-						</div>
-						<div className="modalSection roomOptions">
-							<div className="modalSectionLabel">Room Options</div>
-							<div className="modalSectionSubLabel">other people in this room will see these changes</div>
-							<ConnectedOptionCheckbox
-								label="straight connection lines"
-								onChange={dispatch => e =>
-									dispatch(roomSettingsActions.changeLineType(e.target.checked
-										? LineType.Straight
-										: LineType.Curved,
-									))}
-								valueSelector={state =>
-									selectRoomSettings(state.room).lineType === LineType.Straight
-										? true
-										: false
-								}
-							/>
-						</div>
-					</Modal>
-				}
-			</Fragment>
-		)
-	}
-}
+					<div className="modalSection localOptions">
+						<div className="modalSectionLabel">Local Options</div>
+						<div className="modalSectionSubLabel">won't affect anyone else</div>
+						<ConnectedOption
+							option={AppOptions.showNoteNamesOnKeyboard}
+							label="show note names on keyboard"
+						/>
+						<ConnectedOption
+							option={AppOptions.requireCtrlToScroll}
+							label="require control key to zoom (might be needed by laptop users)"
+						/>
+						<ConnectedOption
+							option={AppOptions.showNoteSchedulerDebug}
+							label="note scheduler debug: enable"
+						/>
+						<ConnectedOption
+							option={AppOptions.renderNoteSchedulerDebugWhileStopped}
+							label="note scheduler debug: keep rendering even when song is stopped"
+						/>
+						<ConnectedOption
+							option={AppOptions.graphics_fancyConnections}
+							label="graphics: enable fancy connections"
+						/>
+						<ConnectedOption
+							option={AppOptions.graphics_ECS}
+							label="graphics: enable ECS animations (sequencer time marker thing)"
+						/>
+						<ConnectedOption
+							option={AppOptions.graphics_expensiveZoomPan}
+							label="graphics: enable expensive/fancy zoom and pan (sharper render, but slower)"
+						/>
+					</div>
+					<div className="modalSection roomOptions">
+						<div className="modalSectionLabel">Room Options</div>
+						<div className="modalSectionSubLabel">other people in this room will see these changes</div>
+						<ConnectedOptionCheckbox
+							label="straight connection lines"
+							onChange={dispatch => e =>
+								dispatch(roomSettingsActions.changeLineType(e.target.checked
+									? LineType.Straight
+									: LineType.Curved,
+								))}
+							valueSelector={state =>
+								selectRoomSettings(state.room).lineType === LineType.Straight
+									? true
+									: false
+							}
+						/>
+					</div>
+				</Modal>
+			}
+		</Fragment>
+	)
+})
