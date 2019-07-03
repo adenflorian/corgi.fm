624bad57a1cb09e3477c57ac110fc0eec1adee48
David Valachovic <David.E.Valachovic@gmail.com>
2019-07-02 19:36:38 -0500

    api tests

--- src/server/api-tests/api.spec.ts
+++ src/server/api-tests/api.spec.ts
@@ -1,10 +1,29 @@
-// import request from 'supertest'
+import * as request from 'supertest'
 import {configureServerStore} from '../../common/redux'
 import {setupExpressApp} from '../setup-express-app'
 
 describe('API Tests', () => {
-	it('should work', async () => {
-		// const result = await request(setupExpressApp(configureServerStore())).get('http://example.com')
-		expect(true).toBeTruthy()
+	describe('/api', () => {
+		describe('/auth', () => {
+			describe('/register', () => {
+				it('should work', async () => {
+					const result = await request(setupExpressApp(configureServerStore())).post('/api/auth/register')
+					expect(result.status).toEqual(200)
+				})
+			})
+			describe('/login', () => {
+				it('should work', async () => {
+					const result = await request(setupExpressApp(configureServerStore())).post('/api/auth/login')
+					expect(result.status).toEqual(200)
+				})
+			})
+		})
+	})
+
+	describe('/newsletter', () => {
+		it('should work', async () => {
+			const result = await request(setupExpressApp(configureServerStore())).get('/newsletter')
+			expect(result.status).toEqual(200)
+		})
 	})
 })
