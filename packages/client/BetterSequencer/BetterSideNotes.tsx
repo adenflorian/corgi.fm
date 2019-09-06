fd48816ab43e7b9113cececefcf2cb4aedbd8237
David Valachovic <David.E.Valachovic@gmail.com>
2019-09-05 19:06:33 -0500

    clicking on side note plays short note

--- packages/client/BetterSequencer/BetterSideNotes.tsx
+++ packages/client/BetterSequencer/BetterSideNotes.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useCallback} from 'react'
+import {Set} from 'immutable'
 import {CssColor} from '@corgifm/common/shamu-color'
 import {betterSideNotesWidth, smallNoteHeight} from '@corgifm/common/BetterConstants'
+import {useDispatch} from 'react-redux'
+import {localActions} from '@corgifm/common/redux'
 import {isWhiteKey} from '../Keyboard/Keyboard'
 
 interface Props {
@@ -26,34 +29,58 @@ export const BetterSideNotes = React.memo(({
 					transform: `translateY(${-panPixelsY}px)`,
 				}}
 			>
-				{rows.map((row, i) => {
-					const isWhite = isWhiteKey(i)
-					const isC = i % 12 === 0
-					const isSmall = noteHeight <= smallNoteHeight
-					return (
-						<div
-							key={row}
-							className="row"
-							style={{
-								height: noteHeight - 1,
-								backgroundColor: isWhite
-									? CssColor.defaultGray
-									: CssColor.panelGrayDark,
-								color: isWhite ? CssColor.panelGrayDark : CssColor.defaultGray,
-								fontWeight: isWhite ? 600 : 400,
-							}}
-						>
-							<div
-								className="rowLabel"
-								style={{
-									opacity: isSmall ? 0 : isC ? 1 : undefined,
-								}}
-							>
-								{row}
-							</div>
-						</div>
-					)
-				})}
+				{rows.map((row, i) =>
+					<BetterSideNote
+						id={id}
+						i={i}
+						key={row}
+						row={row}
+						noteHeight={noteHeight}
+					/>
+				)}
+			</div>
+		</div>
+	)
+})
+
+interface BetterSideNoteProps {
+	id: Id
+	i: number
+	row: string
+	noteHeight: number
+}
+
+export const BetterSideNote = React.memo(({
+	id, i, row, noteHeight,
+}: BetterSideNoteProps) => {
+	const isWhite = isWhiteKey(i)
+	const isC = i % 12 === 0
+	const isSmall = noteHeight <= smallNoteHeight
+	const dispatch = useDispatch()
+	const onClick = useCallback(() => {
+		dispatch(localActions.playShortNote(id, Set([i])))
+	}, [dispatch, i, id])
+	return (
+		<div
+			key={row}
+			className="row"
+			style={{
+				height: noteHeight - 1,
+				backgroundColor: isWhite
+					? CssColor.defaultGray
+					: CssColor.panelGrayDark,
+				fontWeight: isWhite ? 600 : 400,
+			}}
+			onMouseDown={onClick}
+		>
+			<div
+				className="rowLabel"
+				style={{
+					opacity: isSmall ? 0 : isC ? 1 : undefined,
+					color: isWhite ? CssColor.panelGrayDark : CssColor.defaultGray,
+				}}
+			>
+				{row}
 			</div>
 		</div>
 	)
