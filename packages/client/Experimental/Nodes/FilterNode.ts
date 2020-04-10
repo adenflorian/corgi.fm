2c5831847e8c494ca4dc5fdc302e6fc29df9a558
David Valachovic <David.E.Valachovic@gmail.com>
2020-04-10 01:39:37 -0500

    FilterHound

--- packages/client/Experimental/Nodes/FilterNode.ts
+++ packages/client/Experimental/Nodes/FilterNode.ts
@@ -9,43 +9,37 @@ import {CorgiNode, CorgiNodeArgs} from '../CorgiNode'
 import {DryWetChain} from './NodeHelpers/DryWetChain'
 import {LabBiquadFilterNode} from './PugAudioNode/Lab'
 
-const filterTypes = ['lowpass', 'highpass', 'bandpass', 'lowshelf', 'highshelf', 'peaking', 'notch', 'allpass'] as const
+const filterTypes = ['lowpass', 'highpass', 'bandpass', 'lowshelf',
+	'highshelf', 'peaking', 'notch', 'allpass'] as const
 type FilterType = typeof filterTypes[number]
 
 export class FilterNode extends CorgiNode {
 	protected readonly _ports: ExpPorts
 	protected readonly _audioParams: ExpAudioParams
 	protected readonly _customEnumParams: ExpCustomEnumParams
-	private readonly _filter: LabBiquadFilterNode
 	private readonly _dryWetChain: DryWetChain
-	private readonly _type: ExpCustomEnumParam<FilterType>
+	private readonly _filterHound: FilterHound
 
 	public constructor(corgiNodeArgs: CorgiNodeArgs) {
 		super(corgiNodeArgs, {name: 'Filter', color: CssColor.orange})
 
-		this._type = new ExpCustomEnumParam<FilterType>('type', 'lowpass', filterTypes)
-		this._type.onChange.subscribe(this.onTypeChange)
-		this._customEnumParams = arrayToESIdKeyMap([this._type] as ExpCustomEnumParam<string>[])
+		this._filterHound = new FilterHound(corgiNodeArgs)
 
-		this._filter = new LabBiquadFilterNode({...corgiNodeArgs, voiceMode: 'autoPoly', creatorName: 'FilterNode'})
-		this._filter.type = 'lowpass'
+		this._customEnumParams = arrayToESIdKeyMap([this._filterHound.type] as ExpCustomEnumParam<string>[])
 
-		this._dryWetChain = new DryWetChain(corgiNodeArgs.audioContext, this._filter, 'autoPoly')
-
-		const frequencyParam = new ExpAudioParam('frequency', this._filter.frequency, 632, 20000, 'unipolar', {valueString: filterValueToString, curveFunctions: filterFreqCurveFunctions})
-		const detuneParam = new ExpAudioParam('detune', this._filter.detune, 0, 100, 'bipolar', {valueString: filterValueToString})
-		const qParam = new ExpAudioParam('q', this._filter.Q, 1, 18, 'unipolar')
-		const gainParam = new ExpAudioParam('gain', this._filter.gain, 0, 1, 'bipolar', {valueString: percentageValueString})
+		this._dryWetChain = new DryWetChain(corgiNodeArgs.audioContext, this._filterHound.filter, 'autoPoly')
 
 		const inputPort = new ExpNodeAudioInputPort('input', 'input', this, this._dryWetChain.inputGain)
-		const frequencyPort = new ExpNodeAudioParamInputPort(frequencyParam, this, corgiNodeArgs, 'center')
-		const detunePort = new ExpNodeAudioParamInputPort(detuneParam, this, corgiNodeArgs, 'center')
-		const qPort = new ExpNodeAudioParamInputPort(qParam, this, corgiNodeArgs, 'offset')
-		const gainPort = new ExpNodeAudioParamInputPort(gainParam, this, corgiNodeArgs, 'offset')
+		const frequencyPort = new ExpNodeAudioParamInputPort(this._filterHound.frequencyParam, this, corgiNodeArgs, 'center')
+		const detunePort = new ExpNodeAudioParamInputPort(this._filterHound.detuneParam, this, corgiNodeArgs, 'center')
+		const qPort = new ExpNodeAudioParamInputPort(this._filterHound.qParam, this, corgiNodeArgs, 'offset')
+		const gainPort = new ExpNodeAudioParamInputPort(this._filterHound.gainParam, this, corgiNodeArgs, 'offset')
 		const outputPort = new ExpNodeAudioOutputPort('output', 'output', this, this._dryWetChain.outputGain)
 
 		this._ports = arrayToESIdKeyMap([inputPort, frequencyPort, detunePort, qPort, gainPort, outputPort])
-		this._audioParams = arrayToESIdKeyMap([frequencyParam, detuneParam, qParam, gainParam])
+		this._audioParams = arrayToESIdKeyMap([
+			this._filterHound.frequencyParam, this._filterHound.detuneParam,
+			this._filterHound.qParam, this._filterHound.gainParam])
 	}
 
 	public render = () => this.getDebugView()
@@ -54,11 +48,38 @@ export class FilterNode extends CorgiNode {
 	protected _disable = () => this._dryWetChain.dryOnly()
 
 	protected _dispose() {
-		this._filter.disconnect()
+		this._filterHound.dispose()
 		this._dryWetChain.dispose()
+	}
+}
+
+export class FilterHound {
+	public readonly filter: LabBiquadFilterNode
+	public readonly type: ExpCustomEnumParam<FilterType>
+	public readonly frequencyParam: ExpAudioParam
+	public readonly detuneParam: ExpAudioParam
+	public readonly qParam: ExpAudioParam
+	public readonly gainParam: ExpAudioParam
+
+	public constructor(corgiNodeArgs: CorgiNodeArgs) {
+		this.type = new ExpCustomEnumParam<FilterType>('type', 'lowpass', filterTypes)
+		this.type.onChange.subscribe(this.onTypeChange)
+
+		this.filter = new LabBiquadFilterNode({...corgiNodeArgs, voiceMode: 'autoPoly', creatorName: 'FilterNode'})
+		this.filter.type = 'lowpass'
+
+		this.frequencyParam = new ExpAudioParam('frequency', this.filter.frequency, 632, 20000, 'unipolar',
+			{valueString: filterValueToString, curveFunctions: filterFreqCurveFunctions})
+		this.detuneParam = new ExpAudioParam('detune', this.filter.detune, 0, 100, 'bipolar', {valueString: filterValueToString})
+		this.qParam = new ExpAudioParam('q', this.filter.Q, 1, 18, 'unipolar')
+		this.gainParam = new ExpAudioParam('gain', this.filter.gain, 0, 1, 'bipolar', {valueString: percentageValueString})
+	}
+
+	public dispose() {
+		this.filter.disconnect()
 	}
 
 	private readonly onTypeChange = (type: FilterType) => {
-		this._filter.type = type
+		this.filter.type = type
 	}
 }
